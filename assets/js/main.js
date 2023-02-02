49c2eedc1accbc4045dc4f924e56eedb35277413
YusifNezerli <nezerliyusif42@mail.ru>
2023-02-02 15:04:35 +0400

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
-fetch('https://imdb-api.com/API/Search/k_w31e4jog/films')
+document.getElementById('btn').addEventListener('click', () => {
+  document.getElementById('films').innerHTML = ``
+  fetch(`https://imdb-api.com/API/Search/k_w31e4jog/${document.getElementById(`searchText`).value}`)
     .then((resp) => resp.json())
     .then((data) => {
         // console.log(data.results);
         for (let film of data.results) {
             console.log(film);
-            document.getElementById("films").innerHTML += `
+            document.getElementById('films').innerHTML += `
             <div class="row">
             <div class="col-lg-3 col-sm-6" style="width: 100%">
               <div class="item">
@@ -17,7 +19,9 @@ fetch('https://imdb-api.com/API/Search/k_w31e4jog/films')
             `
         }
     })
+  })
 
+   
 
 
 
