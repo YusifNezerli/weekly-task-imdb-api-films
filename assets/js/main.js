c291be3a7ae30f594ed51d9fa0c7611a80b6fba4
YusifNezerli <nezerliyusif42@mail.ru>
2023-02-02 11:33:10 +0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+fetch('https://imdb-api.com/API/Search/k_w31e4jog/films')
+    .then((resp) => resp.json())
+    .then((data) => {
+        // console.log(data.results);
+        for (let film of data.results) {
+            console.log(film);
+            document.getElementById("films").innerHTML += `
+            <div class="row">
+            <div class="col-lg-3 col-sm-6" style="width: 100%">
+              <div class="item">
+                <img class="filmsekil" src="${film.image}" alt="">
+                <h4>${film.title}<br></h4>
+                <h6>${film.description}</h6>
+              </div>
+            </div>
+          </div>
+            `
+        }
+    })
+
+
+
+
+   
